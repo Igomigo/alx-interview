8111385a075848b589731ccecd2892504e3a68bc
Igomigo <igomigofatai@gmail.com>
2023-12-06 05:47:06 +0000

    Pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+""" Recreating the pascal's triangle, but with code, let's have fun """
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    outer = []
+    first_list = [1]
+    outer.append(first_list)
+    for i in range(n - 1):
+        inner = []
+        previous_list = outer[-1]
+        inner.append(1)
+        for x in range(len(previous_list) - 1):
+            next = previous_list[x] + previous_list[x + 1]
+            inner.append(next)
+        inner.append(1)
+        outer.append(inner)
+    return outer
